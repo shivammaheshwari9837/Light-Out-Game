33fcba74ee6e6851f37ab626bcfddcb699d2e6a0
Shivam <shivammaheshwari9837@gmail.com>
2020-04-28 23:01:37 +0530

    Updates

---
+++ precache-manifest.efaad8c88a5e223cc025b05234e0bcdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc56d341e4fd3013b89e",
+    "url": "/Light-Out-Game/static/css/main.c7c9b880.chunk.css"
+  },
+  {
+    "revision": "fc56d341e4fd3013b89e",
+    "url": "/Light-Out-Game/static/js/main.fc56d341.chunk.js"
+  },
+  {
+    "revision": "45ea534042495dbb2393",
+    "url": "/Light-Out-Game/static/js/1.45ea5340.chunk.js"
+  },
+  {
+    "revision": "f029471a51b8f5c9db7f",
+    "url": "/Light-Out-Game/static/js/runtime~main.f029471a.js"
+  },
+  {
+    "revision": "90a954f93f74f661ee4f265afafb9c7c",
+    "url": "/Light-Out-Game/index.html"
+  }
+];
